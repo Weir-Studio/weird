66670b5392bca968db597632d0e6f8bc3ca2f9e5
Weir <Weir@home-weir>
2013-11-11 10:13:00 +0800

    Merge weird-modules/master
    Conflicts:
    	README.md

---
+++ modules/ili-commons/src/main/java/net/ili/commons/App.java
@@ -0,0 +1,13 @@
+package net.ili.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ modules/ili-commons/src/main/java/net/ili/commons/test/SuperSpringTestCase.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.ili.commons.test;
+
+/**
+ * 该类仅作为子项目创建测试用例类父类的实现参考，因Spring已经提供了简洁良好的实现方式，所以不再做封装。
+ *
+ * @author Weir
+ */
+//继承项目中，解开注释并配置ContextConfiguration，ActiveProfiles
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:/spring/spring-main.xml"})
+//@Transactional
+//@ActiveProfiles(profiles = "development")
+public abstract class SuperSpringTestCase {
+}
